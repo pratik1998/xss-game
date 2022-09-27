306bb9f7ab1f3f6a0f905f286b2ea0d8b2de0d7a
Pratik Kayastha <pratikkayastha98@gmail.com>
2022-09-27 14:07:14 -0400

    Adding code for level3

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, request, make_response, render_template
 from level1.main import get_home_page as level1_home_page, get_frame_page as level1_frame_page, get_frame_query_page as level1_frame_query_page, get_source_page as level1_source_page
 from level2.main import get_home_page as level2_home_page, get_frame_page as level2_frame_page, get_source_page as level2_source_page
+from level3.main import get_home_page as level3_home_page, get_frame_page as level3_frame_page, get_source_page as level3_source_page
 from home import get_home_page
 from datetime import datetime, timedelta
 
@@ -68,12 +69,11 @@ def level1_record_feedback():
 # Level 2 routes
 @app.route("/level2", methods=['GET'])
 def level2():
-    level1_cookie = request.cookies.get('level1')
-    print(level1_cookie)
-    print('Level1 Validation: ', validate_cookie('level1', level1_cookie))
-    if level1_cookie and validate_cookie('level1', level1_cookie):
-        return level2_home_page()
-    return render_template('error/error.html')
+    # level1_cookie = request.cookies.get('level1')
+    # if level1_cookie and validate_cookie('level1', level1_cookie):
+    #     return level2_home_page()
+    # return render_template('error/error.html')
+    return level2_home_page()
 
 @app.route("/level2/<path:path>", methods=['GET'])
 def level2_payload_response(path):
@@ -100,35 +100,37 @@ def level2_record_feedback():
     return resp
 
 # Level 3 routes
-@app.route("/level2", methods=['GET'])
-def level2():
-    level1_cookie = request.cookies.get('level1')
-    print(level1_cookie)
-    print('Level1 Validation: ', validate_cookie('level1', level1_cookie))
-    if level1_cookie and validate_cookie('level1', level1_cookie):
-        return level2_home_page()
-    return render_template('error/error.html')
+@app.route("/level3", methods=['GET'])
+def level3():
+    # Skipping cookie validation so we can move between levels
+    # level1_cookie = request.cookies.get('level2')
+    # if level1_cookie and validate_cookie('level1', level1_cookie):
+    #     return level2_home_page()
+    # return render_template('error/error.html')
+    return level3_home_page()
 
-@app.route("/level2/<path:path>", methods=['GET'])
-def level2_payload_response(path):
-    return level2_home_page()
+@app.route("/level3/<path:path>", methods=['GET'])
+def level3_payload_response(path):
+    return level3_home_page()
 
-@app.route("/level2/frame", methods=['GET'])
-def level2_frame():
-    return level2_frame_page()
+@app.route("/level3/frame", methods=['GET'])
+def level3_frame():
+    return level3_frame_page()
 
-@app.route("/level2/source", methods=['GET'])
-def level2_source():
-    return level2_source_page()
+@app.route("/level3/source", methods=['GET'])
+def level3_source():
+    return level3_source_page()
 
-@app.route("/feedback/level2/<path:path>", methods=['GET'])
-def level2_feedback(path):
+@app.route("/feedback/level3/<path:path>", methods=['GET'])
+def level3_feedback(path):
     return 'OK'
 
-@app.route("/level2/record", methods=['GET'])
-def level2_record_feedback():
+@app.route("/level3/record", methods=['GET'])
+def level3_record_feedback():
     resp = make_response('OK')
     cookie_value = generate_cookie()
-    resp.set_cookie('level2', cookie_value, httponly=True, expires=datetime.now() + timedelta(days=30))
-    store_cookie('level2', cookie_value)
+    resp.set_cookie('level3', cookie_value, httponly=True, expires=datetime.now() + timedelta(days=30))
+    store_cookie('level3', cookie_value)
     return resp
+
+# Level 4 routes
---
+++ level3/main.py
@@ -0,0 +1,10 @@
+from flask import render_template
+
+def get_home_page():
+    return render_template('level3/home.html')
+
+def get_frame_page():
+    return render_template('level3/frame.html')
+
+def get_source_page():
+    return render_template('level3/source.html')
