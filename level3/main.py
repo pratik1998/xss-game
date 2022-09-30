65b64504b95c97b3923b9b0a1a3cd277c6f87891
Pratik Kayastha <pratikkayastha98@gmail.com>
2022-09-30 15:43:08 -0400

    Adding csp policy for level3

--- level3/main.py
+++ level3/main.py
@@ -1,10 +1,15 @@
-from flask import render_template
+from flask import render_template, make_response
 
 def get_home_page():
     return render_template('level3/home.html')
 
 def get_frame_page():
-    return render_template('level3/frame.html')
+    resp = make_response(render_template('level3/frame.html'))
+    resp.headers['Content-Security-Policy'] = ("default-src 'self';" +
+      "script-src 'self' ajax.googleapis.com;" +
+      "style-src 'self';"
+    )
+    return resp
 
 def get_source_page():
     return render_template('level3/source.html')
