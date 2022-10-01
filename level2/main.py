a33324d3ec11c0fa70cd9504cb0096ae8b73597a
Pratik Kayastha <pratikkayastha98@gmail.com>
2022-10-01 16:46:01 -0400

    Add csp3.0 policy for level2

--- level2/main.py
+++ level2/main.py
@@ -1,14 +1,18 @@
 from flask import render_template, make_response
+from utils import *
 
 def get_home_page():
     return render_template('level2/home.html')
 
 def get_frame_page():
-    resp = make_response(render_template('level2/frame.html'))
-    resp.headers['Content-Security-Policy'] = ("default-src 'self';" +
-        "script-src 'self';" +
-        "style-src 'self' http://fonts.googleapis.com 'sha256-NT975AHrbM1rOKWHz7wVzG2s9/CKoxikN97C34fq2do=';"
-    )
+    nonces = generate_nonces(3)
+    data = {
+        'gameFrameNonce': nonces[0],
+        'postStoreNonce': nonces[1],
+        'frameNonce': nonces[2]
+    }
+    resp = make_response(render_template('level2/frame.html', nonce_data=data))
+    resp.headers['Content-Security-Policy'] = f'script-src {generate_nonce_str_from_list(nonces)};'
     return resp
 
 def get_source_page():
