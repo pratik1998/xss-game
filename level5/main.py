c4627fbc093c0ce51d32727d0015e691a3544173
Pratik Kayastha <pratikkayastha98@gmail.com>
2022-10-01 17:37:28 -0400

    Add csp3.0 policy for level5

--- level5/main.py
+++ level5/main.py
@@ -1,15 +1,13 @@
 from flask import render_template, make_response
+from utils import generate_nonce, generate_nonces, generate_nonce_str_from_list
 
 def get_home_page():
     return render_template('level5/home.html')
 
 def get_frame_page():
-    resp = make_response(render_template('level5/frame.html'))
-    resp.headers['Content-Security-Policy'] = (
-      "default-src 'self';" +
-      "script-src 'self';" +
-      "style-src 'self';"
-    )
+    nonce = generate_nonce()
+    resp = make_response(render_template('level5/frame.html', nonce=nonce))
+    resp.headers['Content-Security-Policy'] = f'script-src {generate_nonce_str_from_list([nonce])};'
     return resp
 
 def get_source_page():
@@ -19,19 +17,17 @@ def get_signup_page(signup):
     # ALLOWED_NEXT_URLS = ['confirm']
     # if signup not in ALLOWED_NEXT_URLS:
     #     signup = 'confirm'
-    resp = make_response(render_template('level5/signup.html', next=signup))
-    resp.headers['Content-Security-Policy'] = (
-      "default-src 'self';" +
-      "script-src 'self';" +
-      "style-src 'self';"
-    )
+    nonce = generate_nonce()
+    resp = make_response(render_template('level5/signup.html', next=signup, nonce=nonce))
+    resp.headers['Content-Security-Policy'] = f'script-src {generate_nonce_str_from_list([nonce])};'
     return resp
 
 def get_confirmation_page(next):
-    resp = make_response(render_template('level5/confirm.html', next=next))
-    resp.headers['Content-Security-Policy'] = (
-      "default-src 'self';" +
-      "script-src 'self';" +
-      "style-src 'self';"
-    )
+    nonces = generate_nonces(2)
+    data = {
+        'gameFrameNonce': nonces[0],
+        'confirmNonce': nonces[1]
+    }
+    resp = make_response(render_template('level5/confirm.html', next=next, nonce_data=data))
+    resp.headers['Content-Security-Policy'] = f'script-src {generate_nonce_str_from_list(nonces)};'
     return resp
