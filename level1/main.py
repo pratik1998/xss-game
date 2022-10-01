25d05a0ff0e481147e87c7c6c82483aa2d9ec3d6
Pratik Kayastha <pratikkayastha98@gmail.com>
2022-10-01 00:35:04 -0400

    Adding csp3.0 with nonce for level1

--- level1/main.py
+++ level1/main.py
@@ -1,4 +1,5 @@
 import os
+from utils import *
 from flask import render_template, make_response, escape
 
 page_header = """
@@ -6,7 +7,7 @@ page_header = """
 <html>
   <head>
     <!-- Internal game scripts/styles, mostly boring stuff -->
-    <script src="/static/game-frame.js"></script>
+    <script nonce="{nonce}" src="/static/game-frame.js"></script>
     <link rel="stylesheet" href="/static/game-frame-styles.css" />
   </head>
  
@@ -26,32 +27,26 @@ main_page_markup = """
   <input id="query" name="query" value="Enter query here...">
   <input id="button" type="submit" value="Search">
 </form>
-<script type="text/javascript" src="/static/level1/frame.js"></script>
+<script nonce="{nonce}" type="text/javascript" src="/static/level1/frame.js"></script>
 """
 
 def get_home_page():
     return render_template('level1/home.html')
 
 def get_frame_page():
-    resp = make_response(page_header + main_page_markup + page_footer)
-    resp.headers['Content-Security-Policy'] = (
-      "default-src 'self';" +
-      "script-src 'self';" +
-      "style-src 'self' http://fonts.googleapis.com;"
-    )
+    nonces = generate_nonces(2)
+    resp = make_response(page_header.format(nonce=nonces[0]) + main_page_markup.format(nonce=nonces[1]) + page_footer)
+    resp.headers['Content-Security-Policy'] = f'script-src {generate_nonce_str_from_list(nonces)};'
     return resp
 
 def get_frame_query_page(query):
     # escaped_query = escape(query)
     escaped_query = query
+    nonce = generate_nonce()
     message = f"Sorry, no results were found for <b> {escaped_query} </b>."
     message += " <a href='?'>Try again</a>."
-    resp = make_response(page_header + message + page_footer)
-    resp.headers['Content-Security-Policy'] = (
-      "default-src 'self';" +
-      "script-src 'self';" +
-      "style-src 'self' http://fonts.googleapis.com;"
-    )
+    resp = make_response(page_header.format(nonce=nonce) + message + page_footer)
+    resp.headers['Content-Security-Policy'] = f'script-src {generate_nonce_str_from_list([nonce])};'
     return resp
 
 def get_source_page():
